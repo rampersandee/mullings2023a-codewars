24382efc52cce07a392e0aed9689c40dac5ce68e
rampersandee <brandon.j.mullings@gmail.com>
2023-03-24 18:15:44 -0400

    second commit

---
+++ codewars4.js
@@ -0,0 +1,14 @@
+// Write a function "greet" that returns "hello world!"
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. 
+//But how clever can you be to create the most creative "hello world" you can think of? 
+//What is a "hello world" solution you would want to show your friends?
+
+function greet()
+{
+  let greeting = 'h' + 'e' + 'l' + 'l' + 'o' + ' ' + 'w' + 'o' + 'r' + 'l' + 'd' + '!'
+  return greeting
+}
