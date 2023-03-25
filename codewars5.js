960677135040065fa0b0db9404a331333044964a
rampersandee <brandon.j.mullings@gmail.com>
2023-03-25 13:49:17 -0400

    previously unpushed code

---
+++ codewars5.js
@@ -0,0 +1,16 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  if (bool != true)
+    {
+      return 'No'
+    }
+  else if (bool != false)
+    {
+      return 'Yes'
+    }
+  else
+    {
+      return 'the slab'
+    }
+}
