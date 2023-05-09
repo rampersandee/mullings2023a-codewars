be0ce31dc6fcbb32e060cc8f08b257f5daa46d31
Brandon Mullings (Randy) <126500708+rampersandee@users.noreply.github.com>
2023-05-09 18:10:13 -0400

    05/09/23

---
+++ codewars39.js
@@ -0,0 +1,20 @@
+// In this kata, we will make a function to test whether a period is late.
+
+// Our function will take three parameters:
+
+// last - The Date object with the date of the last period
+
+// today - The Date object with the date of the check
+
+// cycleLength - Integer representing the length of the cycle in days
+
+// Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
+
+function periodIsLate(last, today, cycleLength) {
+  let difference = (today - last) / (1000 * 60 * 60 * 24); // Calculate the difference in days
+  if (difference > cycleLength) {
+    return true;
+  } else {
+    return false;
+  }
+}
